8356fba2fc8195b0f59a332bf808d4882c06c39b
aoelvp94 <aoelvp94@gmail.com>
2024-04-16 07:43:25 -0300

    fix query

--- dbt_project/models/example/example.sql
+++ dbt_project/models/example/example.sql
@@ -1,8 +1,9 @@
 SELECT
-    CURRENT_TIMESTAMP() as date,
-    daily_data.symbol,
-    daily_data.avg_price / exchange_rates.value_usd AS avg_price_at_currency,
-    exchange_rates.currency_code
+    'MSFT' AS symbol,
+    (daily_data._2__high + daily_data._3__low / 2 ) / exchange_rates.value_usd AS avg_price_at_currency,
+    (daily_data._5__volume / 1440) AS avg_num_trades,
+    exchange_rates.currency_code,
+    CURRENT_TIMESTAMP() AS processing_date
 FROM
     {{ source(
         'source_data',
