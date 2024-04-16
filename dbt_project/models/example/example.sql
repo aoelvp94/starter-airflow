cec22c1135627fad5179881a68f485c6b8a30c96
aoelvp94 <aoelvp94@gmail.com>
2024-04-16 07:29:55 -0300

    small fix

--- dbt_project/models/example/example.sql
+++ dbt_project/models/example/example.sql
@@ -1,6 +1,6 @@
 SELECT
     CURRENT_TIMESTAMP() as date,
-    symbol,
+    daily_data.symbol,
     daily_data.avg_price / exchange_rates.value_usd AS avg_price_at_currency,
     exchange_rates.currency_code
 FROM
