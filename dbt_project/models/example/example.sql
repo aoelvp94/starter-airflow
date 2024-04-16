3f04fc82bae69259b2ab366d6a24f09a9a8a460a
aoelvp94 <aoelvp94@gmail.com>
2024-04-16 08:10:53 -0300

    fix tests

--- dbt_project/models/example/example.sql
+++ dbt_project/models/example/example.sql
@@ -3,6 +3,7 @@ SELECT
     ((CAST(daily_data._2__high AS FLOAT64) + CAST(daily_data._3__low AS FLOAT64)) / 2 ) / exchange_rates.value_usd AS avg_price_at_currency,
     (CAST(daily_data._5__volume AS INT64) / 1440) AS avg_num_trades,
     exchange_rates.currency_code,
+    _airbyte_extracted_at AS ingestion_date,
     CURRENT_TIMESTAMP() AS processing_date
 FROM
     {{ source(
